95f181d7cd333f9cfeb7d66a07eb5afab4064d04
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2024-01-17 22:17:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
   // write your code here
+	let str1 = text.toLowerCase();
+	let str2 = text.toUpperCase();
+	return str1.concat("-",str2);
 }
 
-// DO not change the code below
-
 const text = prompt("Enter text:");
+
 alert(toCase(text));
+
